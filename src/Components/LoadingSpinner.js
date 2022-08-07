cbf16be06d72fdf2d1ea3edadc771010fdd63a8c
sanjay-karki <saanu.karki97@gmail.com>
2022-08-07 15:06:53 +0545

    Optimized hovers + minor style changes

--- src/Components/LoadingSpinner.js
+++ src/Components/LoadingSpinner.js
@@ -4,7 +4,7 @@ import "./spinner.css";
 export default function LoadingSpinner() {
   return (
     <div className="spinner-container">
-      <div className="loading-text">Fetching questions. . .</div>
+      <div className="loading-text">Fetching questions</div>
       <div className="loading-spinner"></div>
     </div>
   );
